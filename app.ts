8e0e9a754f51518748d7af748319854e850e835c
Matthew C Wake <mcwake-dev@outlook.com>
2022-09-21 11:28:05 +0100

    Added CORS support

--- app.ts
+++ app.ts
@@ -1,7 +1,9 @@
 import { Application } from "https://deno.land/x/oak/mod.ts";
+import { oakCors } from "https://deno.land/x/cors/mod.ts";
 
 const app = new Application();
 
+app.use(oakCors);
 app.use((ctx) => {
     ctx.response.body = "Hello World!";
 })
